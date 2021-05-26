4f549e7d58c47068d2f340b60d073e1dfbc34cbe
Mattia Righetti <matt95.righetti@gmail.com>
2021-05-26 03:23:11 +0200

    Removed user_survey_section

--- ddl/db_schema.sql
+++ ddl/db_schema.sql
@@ -130,20 +130,6 @@ CREATE TABLE `question_option`(
         ON UPDATE CASCADE ON DELETE NO ACTION
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `user_survey_sections`;
-
-CREATE TABLE `user_survey_sections` (
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` INT UNSIGNED NOT NULL,
-    `survey_section_id` INT UNSIGNED NOT NULL,
-    `completed_on` INT UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
-        ON UPDATE CASCADE ON DELETE NO ACTION,
-    FOREIGN KEY (`survey_section_id`) REFERENCES `survey_section`(`id`)
-        ON UPDATE CASCADE ON DELETE NO ACTION
-) ENGINE=InnoDB;
-
 DROP TABLE IF EXISTS `answer`;
 
 CREATE TABLE `answer` (
